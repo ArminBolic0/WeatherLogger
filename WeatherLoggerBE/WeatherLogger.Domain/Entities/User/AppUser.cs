963273706d8c0ede4a8abe85c13664ee61a61970
Armin Bolic <armin.bolaa@gmail.com>
2026-01-31 20:58:35 +0100

    Remove Id property from AppUser class

--- WeatherLoggerBE/WeatherLogger.Domain/Entities/User/AppUser.cs
+++ WeatherLoggerBE/WeatherLogger.Domain/Entities/User/AppUser.cs
@@ -2,7 +2,6 @@
 {
     public class AppUser : BaseEntity
     {
-        public int Id { get; set; }
         public string GoogleId { get; set; } = null!;
         public ICollection<UserCityHistory> CityHistories { get; set; } = new List<UserCityHistory>();
     }
